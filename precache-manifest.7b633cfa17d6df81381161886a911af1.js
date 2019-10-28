b12a119eb281dcfc96260db311a387dec2d53dd6
Gema Muhammad Reza <gema@skyshi.com>
2019-10-28 16:16:10 +0700

    Updates

---
+++ precache-manifest.7b633cfa17d6df81381161886a911af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a04eb384415a68fc9d124a2032ae25d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8546a80a268b2e374dde",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "762c42a602bdc34b8391",
+    "url": "/portfolio/static/js/2.523f1d7f.chunk.js"
+  },
+  {
+    "revision": "8546a80a268b2e374dde",
+    "url": "/portfolio/static/js/main.d8c264f8.chunk.js"
+  },
+  {
+    "revision": "1b4b9b81f91bf96f5353",
+    "url": "/portfolio/static/js/runtime-main.3654f6ad.js"
+  }
+]);
